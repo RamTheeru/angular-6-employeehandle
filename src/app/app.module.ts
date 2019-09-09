4f58f7415a5921d786242f4d82b3a0d06aad8a85
RamTheeru <theertha808@gmail.com>
2019-09-09 15:58:21 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {MaterialModule} from './employees/material/material.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeService } from './employees/shared/employee.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,MaterialModule ],
   declarations: [ AppComponent, HelloComponent, EmployeesComponent, EmployeeComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [EmployeeService]
 })
 export class AppModule { }
