3fea140aef12c770a0764f3d635c941b9e9e1a40
RamTheeru <theertha808@gmail.com>
2019-09-10 15:25:54 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import {MaterialModule} from './employees/material/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeService } from './employees/shared/employee.service';
+import {environment} from './environment';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,MaterialModule,ReactiveFormsModule,BrowserAnimationsModule ],
+  imports:      [ BrowserModule, FormsModule,MaterialModule,ReactiveFormsModule,BrowserAnimationsModule,AngularFireDatabaseModule,AngularFireModule.initializeApp(environment.firebaseConfig) ],
   declarations: [ AppComponent, HelloComponent, EmployeesComponent, EmployeeComponent ],
   bootstrap:    [ AppComponent ],
   providers: [EmployeeService]
---
+++ src/app/environment.ts
@@ -0,0 +1,12 @@
+export const environment ={
+  production :false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAprLamVn4NbScsY9yQ-Lr00jHsyCy9iuo",
+    authDomain: "amcrud-15b39.firebaseapp.com",
+    databaseURL: "https://amcrud-15b39.firebaseio.com",
+    projectId: "amcrud-15b39",
+    storageBucket: "",
+    messagingSenderId: "846016734446",
+    appId: "1:846016734446:web:cf759830228613213cbfff"
+  }
+};
